6d88f83b6c06c49292aea09c234a0974a91c7bbd
J. Nathan Matias <natematias@gmail.com>
2012-03-17 09:12:18 -0400

    added method to get next article, refactored common loadNextMonth method

--- src/nytimes_article_accessor.py
+++ src/nytimes_article_accessor.py
@@ -5,21 +5,35 @@ class NYTimesArticleAccessor:
   def __init__(self, data_dir):
     self.data_dir = data_dir
     self.csv_filenames = glob.glob(data_dir + "/*")
+    self.csv_filenames.sort()
     self.current_file = None
     self.nyt_csv_reader = None
 
   def getNextArticle(self):
-    print "Accessing ArticleAccessor: Please Access Inherited Class"
+    if self.nyt_csv_reader == None:
+      load_success = self.loadNextMonth()
+      if not load_success:
+        return None
+    try:
+      return self.nyt_csv_reader.next()
+    except StopIteration:
+      self.nyt_csv_reader = None
+      return self.getNextArticle()
 
   def getNextMonth(self):
-    self.current_file = 0 if self.current_file == None else self.current_file + 1
-    if self.current_file != None and self.current_file >= len(self.csv_filenames):
+    load_success = self.loadNextMonth()
+    if not load_success:
       return None
-
-    current_filename = self.csv_filenames[self.current_file]
-    self.nyt_csv_reader = csv.reader(open(current_filename, 'rb'))
     rows = []
     for row in self.nyt_csv_reader:
       rows.append(row)
     self.nyt_csv_reader = None
     return rows
+
+  def loadNextMonth(self):
+    self.current_file = 0 if self.current_file == None else self.current_file + 1
+    if self.current_file != None and self.current_file >= len(self.csv_filenames):
+      return None
+    current_filename = self.csv_filenames[self.current_file]
+    self.nyt_csv_reader = csv.reader(open(current_filename, 'rb'))
+    return 1
--- test/unit/nytimes_article_accessor_test.py
+++ test/unit/nytimes_article_accessor_test.py
@@ -4,6 +4,7 @@ from nytimes_article_accessor import *
 class NYTimesArticleAccessorTest(unittest.TestCase):
   def setUp(self):
     self.article_accessor = NYTimesArticleAccessor("test/fixtures/nytimes")
+
   def testGetNextMonth(self):
     month = self.article_accessor.getNextMonth()
     self.assertEqual(20, len(month))
@@ -11,6 +12,11 @@ class NYTimesArticleAccessorTest(unittest.TestCase):
     self.assertEqual(19, len(month))
     month = self.article_accessor.getNextMonth()
     self.assertEqual(None, month)
+
+  def testGetNextArticle(self):
+    for i in range(39):
+      assert self.article_accessor.getNextArticle() != None, 'iterating through next articles, ' + str(i) + ' returned None'
+    self.assertEqual(None, self.article_accessor.getNextArticle())
 
 if __name__ == '__main__':
   unittest.main()
