82148cf222b9849f2092d4e60d777f2ab13ff37d
J. Nathan Matias <natematias@gmail.com>
2012-04-10 00:28:12 -0400

    updated sample_test_case so that it would successfully load nytimes articles with fulltext into the test database, using fixture data

--- test/unit/sample_test_case.py
+++ test/unit/sample_test_case.py
@@ -1,3 +1,6 @@
+import config
+config.PUGG_DB="pugg_test"
+
 import unittest
 import sample_class
 import test_common 
@@ -5,13 +8,16 @@ from mongo_connection import *
 
 class SampleClassTest(unittest.TestCase):
   def setUp(self):
-    test_common.CommonSetup.setupMongoDBFixtures()
+    test_common.CommonSetup.setupMediaCloudFixtures()
+    test_common.CommonSetup.setupNYTimesFixtures()
    
   def tearDown(self):
-    test_common.CommonSetup.teardownMongoDBFixtures()
+    test_common.CommonSetup.teardownMediaCloudFixtures()
+    test_common.CommonSetup.teardownNYTimesFixtures()
 
   def testSetupTeardown(self):
     self.assertEqual(5, MONGO_DB.mc_articles.count())
+    self.assertEqual(39, MONGO_DB.articles.count())
 
 test_common.ALL_TESTS.append(SampleClassTest)
 
--- test/unit/test_common.py
+++ test/unit/test_common.py
@@ -1,4 +1,5 @@
 import config
+from nytimes_article_accessor import *
 
 config.PUGG_DB="pugg_test"
 ALL_TESTS=[]
@@ -9,12 +10,29 @@ import json
 
 class CommonSetup:
   @staticmethod
-  def setupMongoDBFixtures():
+  def setupMediaCloudFixtures():
     #load MediaCloud articles into mc_articles
     mongo_fixtures = json.load(open("test/fixtures/mediacloud/mediacloud_articles.json", "r"))
     for mongo_fixture in mongo_fixtures:
       MONGO_DB.mc_articles.insert(mongo_fixture)
   @staticmethod
-  def teardownMongoDBFixtures():
+  def teardownMediaCloudFixtures():
     MONGO_DB.mc_articles.remove()
       
+  @staticmethod
+  def setupNYTimesFixtures():
+    article_accessor = NYTimesArticleAccessor("test/fixtures/nytimes")
+    article = article_accessor.createArticle(article_accessor.getNextArticle())
+    while(article):
+      article.fulltext = article.getDataFileObject("data/full/", "test/fixtures/nytimes-data/", "txt").read()
+      article.save()
+      try:
+        article = article_accessor.createArticle(article_accessor.getNextArticle())
+      except ValueError:
+        continue
+      except TypeError:
+        break
+
+  @staticmethod
+  def teardownNYTimesFixtures():
+    MONGO_DB.articles.remove()
