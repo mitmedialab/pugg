c8a6a36841186e9c5aed78570bdc15dc138925ce
J. Nathan Matias <natematias@gmail.com>
2012-04-09 22:48:07 -0400

    updated nytimes article accessor test to account for dud header row in rows file

--- test/unit/nytimes_article_accessor_test.py
+++ test/unit/nytimes_article_accessor_test.py
@@ -10,14 +10,14 @@ class NYTimesArticleAccessorTest(unittest.TestCase):
 
   def testGetNextMonth(self):
     month = self.article_accessor.getNextMonth()
-    self.assertEqual(20, len(month))
+    self.assertEqual(21, len(month))
     month = self.article_accessor.getNextMonth()
     self.assertEqual(19, len(month))
     month = self.article_accessor.getNextMonth()
     self.assertEqual(None, month)
 
   def testGetNextArticle(self):
-    for i in range(39):
+    for i in range(40):
       assert self.article_accessor.getNextArticle() != None, 'iterating through next articles, ' + str(i) + ' returned None'
     self.assertEqual(None, self.article_accessor.getNextArticle())
 
