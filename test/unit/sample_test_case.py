ac6b4c3c980b1a86333a02fdecfc58cfa137f3dd
J. Nathan Matias <natematias@gmail.com>
2012-04-09 21:54:16 -0400

    added setup and teardown methods for mediacloud + mongodb article fixtures

--- test/unit/sample_test_case.py
+++ test/unit/sample_test_case.py
@@ -1,10 +1,17 @@
 import unittest
 import sample_class
-import test_common
+import test_common 
+from mongo_connection import *
 
 class SampleClassTest(unittest.TestCase):
-  def runTest(self):
-    assert 1
+  def setUp(self):
+    test_common.CommonSetup.setupMongoDBFixtures()
+   
+  def tearDown(self):
+    test_common.CommonSetup.teardownMongoDBFixtures()
+
+  def testSetupTeardown(self):
+    self.assertEqual(5, MONGO_DB.mc_articles.count())
 
 test_common.ALL_TESTS.append(SampleClassTest)
 
--- test/unit/test_common.py
+++ test/unit/test_common.py
@@ -1,1 +1,20 @@
+import config
+
+config.PUGG_DB="pugg_test"
 ALL_TESTS=[]
+
+from mongo_connection import *
+import json
+
+
+class CommonSetup:
+  @staticmethod
+  def setupMongoDBFixtures():
+    #load MediaCloud articles into mc_articles
+    mongo_fixtures = json.load(open("test/fixtures/mediacloud/mediacloud_articles.json", "r"))
+    for mongo_fixture in mongo_fixtures:
+      MONGO_DB.mc_articles.insert(mongo_fixture)
+  @staticmethod
+  def teardownMongoDBFixtures():
+    MONGO_DB.mc_articles.remove()
+      
