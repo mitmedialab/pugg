e7421db5acacfd5aa91eb21d0f74bc0162567c91
J. Nathan Matias <natematias@gmail.com>
2012-07-26 14:33:36 +0100

    added script to get share data for telegraph articles

--- utilities/get_share_data_for_article.rb
+++ utilities/get_share_data_for_article.rb
@@ -1,19 +1,23 @@
 require 'date'
 require 'hpricot'
 require 'open-uri'
+require 'rest_client'
 require 'pp'
 require 'json'
 
 index_data = JSON.load(File.open(ARGV[0]))
 error_counter = 0
-counter = 10000
+counter = 500
 
 index_data.each do |date, articles|
   articles.each_index do |index|
     counter -=1
     break if counter <=0
     next if articles[index].has_key? "sharedata"
-    url = "http://localhost:1337/?q=" + "http://telegraph.co.uk" + articles[index]["url"]
+    basepath = ""
+    basepath ="http://telegraph.co.uk" if(!articles[index]["url"].match(/http/))
+    url = "http://localhost:1337/?q=" + basepath + articles[index]["url"]
+
     begin
       sharedata = JSON.parse(URI.parse(url).read)
     rescue  Exception => e
@@ -29,6 +33,7 @@ index_data.each do |date, articles|
     end
     print "."
     index_data[date][index]["sharedata"] = sharedata
+    RestClient.put 'http://localhost:5984/', jdata, {:content_type => :json}
     error_counter = 0
     sleep(1.0/8.0)
   end
