a517bb77adb3684499edfa910276688570f47aa9
J. Nathan Matias <natematias@gmail.com>
2012-08-02 11:34:49 -0400

    updated code

--- utilities/download_telegraph_content.rb
+++ utilities/download_telegraph_content.rb
@@ -4,36 +4,54 @@ require 'hpricot'
 require 'open-uri'
 require 'pp'
 require 'json'
+require 'iconv'
 
 index_data = JSON.load(File.open(ARGV[0]))
 error_counter = 0
-counter = 5000
+counter = 25000
+
+ic = Iconv.new('UTF-8//IGNORE', 'UTF-8')
+
 
 index_data.each do |date, articles|
   articles.each_index do |index|
     counter -=1
     break if counter <=0
-    next if articles[index].has_key? "sharedata"
+    next if articles[index].has_key? "fulltext"
+    filename = date + "_" + index.to_s
+    if(File.exists? "data/telegraph/articles/#{filename}.html")
+      index_data[date][index]["fulltext"] = filename
+      error_counter = 0
+      print "e"
+      next
+    end
+
+    
     basepath = ""
     basepath ="http://telegraph.co.uk" if(!articles[index]["url"].match(/http/))
     url = basepath + articles[index]["url"]
 
     begin
-      article = Hpricot::XML(URI.parse(url).read)
+      text = URI.parse(url).read
+      text = ic.iconv(text + ' ')[0..-2]
+
+      article = Hpricot::XML(text)
       article_text = (article/'div.twoThirds')[0].inner_html
+      article_text = article if article_text.nil?
+      #article_text = URI.parse(url).read
     rescue  Exception => e
-      print "x"
       sleep(2)
       if e.message=="Timeout::Error"
+        print "X"
         error_counter += 1
         redo if error_counter <= 3
       else
+        print "x"
         error_counter = 0
         next
       end
     end
     print "."
-    filename = date + "_" + index.to_s
     File.open("data/telegraph/articles/#{filename}.html", "wb"){|f|
       f.write article_text
     }
