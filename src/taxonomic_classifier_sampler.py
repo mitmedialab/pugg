192053feea31150c01200fa8c5970c9cf2141d37
Sophie Diehl <sophiadiehl@gmail.com>
2012-08-09 17:22:55 -0500

    Searching taxonomic classifiers

--- src/taxonomic_classifier_sampler.py
+++ src/taxonomic_classifier_sampler.py
@@ -26,6 +26,7 @@ class NYTimesTaxClassSampler:
     def generate_classifier_count(self):
         obit_count = 0
         movies_obit_count = 0
+        death_notice_count = 0
         
         article_row = self.articles.getNextArticle()
         
@@ -40,10 +41,13 @@ class NYTimesTaxClassSampler:
                   obit_count += 1
               if tax_class == "Top/Features/Movies/News and Features/Obituaries":
                   movies_obit_count += 1
+              if tax_class == "Top/Classifieds/Paid Death Notices":
+                  death_notice_count += 1
           article_row = self.articles.getNextArticle()
 
         print "Top/News/Obituaries: " + str(obit_count)
         print "Top/Features/Movies/News and Features/Obituaries: " + str(movies_obit_count)
+        print "Top/Classifieds/Paid Death Notices: " + str(death_notice_count)
 
 if __name__ == "__main__":
     nyt_tax_class_sampler = NYTimesTaxClassSampler()
