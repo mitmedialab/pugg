deb2dffbf598e547eb018978532d9ca68a7deeaf
Sophie Diehl <sophiadiehl@gmail.com>
2012-08-09 17:13:38 -0500

    Added method for counting number of occurrences of particular taxonomic classifiers.

--- src/taxonomic_classifier_sampler.py
+++ src/taxonomic_classifier_sampler.py
@@ -8,7 +8,6 @@ class NYTimesTaxClassSampler:
     def generate_tax_class_samples(self):
         tax_class_samples = []
         article_row = self.articles.getNextArticle()
-        year = 0
         
         while article_row:
           try:
@@ -24,6 +23,28 @@ class NYTimesTaxClassSampler:
         for tax_class in tax_class_samples:
             print tax_class
 
+    def generate_classifier_count(self):
+        obit_count = 0
+        movies_obit_count = 0
+        
+        article_row = self.articles.getNextArticle()
+        
+        while article_row:
+          try:
+              article = self.articles.createArticle(article_row)
+          except ValueError:
+              article_row = self.articles.getNextArticle()
+              continue
+          for tax_class in article.taxonomic_classifiers:
+              if tax_class == "Top/News/Obituaries":
+                  obit_count += 1
+              if tax_class == "Top/Features/Movies/News and Features/Obituaries":
+                  movies_obit_count += 1
+          article_row = self.articles.getNextArticle()
+
+        print "Top/News/Obituaries: " + str(obit_count)
+        print "Top/Features/Movies/News and Features/Obituaries: " + str(movies_obit_count)
+
 if __name__ == "__main__":
     nyt_tax_class_sampler = NYTimesTaxClassSampler()
-    nyt_tax_class_sampler.generate_tax_class_samples()
+    nyt_tax_class_sampler.generate_classifier_count()
