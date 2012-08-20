a5fdf0bb69dd1b55426f7c95ab9ded0fc0294863
Sophie Diehl <sophiadiehl@gmail.com>
2012-08-20 15:24:54 -0500

    Have to get fulltexts back onto pugg server

--- src/ngram_counter.py
+++ src/ngram_counter.py
@@ -1,5 +1,6 @@
 from nltk.tokenize import word_tokenize, wordpunct_tokenize, sent_tokenize
 from string import *
+import re
 
 class WordCounter: # Looks for single words in article fulltexts
     def __init__(self, phrases, papa_dir):
@@ -36,9 +37,9 @@ class WordCounter: # Looks for single words in article fulltexts
       # Must access using dict[phrase][year-1987]
         line = "year,total_fem_obit"
         for phrase in self.phrases:
-            self.phrase_fulltext_dict[phrase] = \
-                [open(self.papa_dir+'/results/'+phrase+'_'+str(i)+'.txt', 'w')\
-                 for i in range(1987, 2007)]
+        ###    self.phrase_fulltext_dict[phrase] = \
+        ###        [open(self.papa_dir+'/results/'+phrase+'_'+str(i)+'.txt', 'w')\
+        ###         for i in range(1987, 2007)]
             line = line +','+phrase+'_count'+','+phrase+'_rate'
 
         print line
@@ -54,10 +55,12 @@ class WordCounter: # Looks for single words in article fulltexts
                             # For now, each article can only count once
                             if not self.phrase_bools_count_rate[word][0]:
                                 self.phrase_bools_count_rate[word][0] = True
-                                self.phrase_bools_count_rate[word][1] += 1
-                                self.phrase_fulltext_dict[word][year-1987].write(re.sub('\n', ' ', fulltext)+'\n')
+                                ###self.phrase_fulltext_dict[word][year-1987].write(re.sub('\n', ' ', fulltext)+'\n')
                 current_file.close()
+                # Increment phrase count and reset phrase bool to false
                 for phrase in self.phrases:
+                    if self.phrase_bools_count_rate[word][0]:
+                        self.phrase_bools_count_rate[word][1] += 1
                     self.phrase_bools_count_rate[phrase][0] = False
 
             # Calculate and store phrase-use-rates
@@ -65,6 +68,7 @@ class WordCounter: # Looks for single words in article fulltexts
                 self.phrase_bools_count_rate[phrase][2] = \
                         self.phrase_bools_count_rate[phrase][1] / \
                         float(self.yearly_female_obit_totals[year])
+                ###self.phrase_fulltext_dict[phrase][year-1987].close()
 
             # Print counts & rates and reset all count variables to 0
             line = str(year)+','+str(self.yearly_female_obit_totals[year])
@@ -74,8 +78,22 @@ class WordCounter: # Looks for single words in article fulltexts
                 self.phrase_bools_count_rate[phrase] = [False, 0, 0]
                 
             print line
-            
+
+    def count_total_words(self):
+        print "year,word_total,avg_obit_length_in_words"
+        for year in range(1987, 2007):
+            word_total = 0
+            for article_index in range(self.yearly_female_obit_totals[year]):
+                current_file = open(self.papa_dir+'/'+str(year)+'/'+str(article_index)+'.txt', 'r')
+                fulltext = current_file.read()
+                for sentence in sent_tokenize(fulltext):
+                    for word in word_tokenize(sentence):
+                        word_total += 1
+                        current_file.close()
+            print str(year)+','+str(word_total)+','+str(word_total/float(self.yearly_female_obit_totals[year]))
+
+                
             
 if __name__ == "__main__":
     word_counter = WordCounter(["married", "founded", "research"], "obits_fem")
-    word_counter.count_words()
+    word_counter.count_total_words()
